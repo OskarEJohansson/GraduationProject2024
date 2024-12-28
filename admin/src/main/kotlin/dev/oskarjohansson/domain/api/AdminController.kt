25cc374452b43fd4c5eac5faab20eb461c62499f
Oskar Johanssonlsclear <133226269+OskarEJohansson@users.noreply.github.com>
2024-12-28 14:28:23 +0100

    Implemented pageable on get users in Admin

--- admin/src/main/kotlin/dev/oskarjohansson/domain/api/AdminController.kt
+++ admin/src/main/kotlin/dev/oskarjohansson/domain/api/AdminController.kt
@@ -4,6 +4,8 @@ import dev.oskarjohansson.api.dto.response.ResponseDTO
 import dev.oskarjohansson.domain.service.AdminService
 import dev.oskarjohansson.model.User
 import jakarta.servlet.http.HttpServletRequest
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -15,13 +17,13 @@ import org.springframework.web.bind.annotation.RestController
 class AdminController(private val adminService: AdminService) {
 
     @GetMapping("/users")
-    fun users(httpServletRequest: HttpServletRequest): ResponseEntity<ResponseDTO<List<User>>> {
+    fun users(httpServletRequest: HttpServletRequest, pageable: Pageable): ResponseEntity<ResponseDTO<Page<User>>> {
         return try {
             ResponseEntity.status(HttpStatus.OK).body(
                 ResponseDTO(
                     HttpStatus.OK.value(),
                     "Users",
-                    adminService.getUsers())
+                    adminService.getUsers(pageable))
             )
         } catch (exception: IllegalStateException) {
             ResponseEntity.internalServerError().body(
--- admin/src/main/kotlin/dev/oskarjohansson/domain/service/AdminService.kt
+++ admin/src/main/kotlin/dev/oskarjohansson/domain/service/AdminService.kt
@@ -2,15 +2,15 @@ package dev.oskarjohansson.domain.service
 
 import dev.oskarjohansson.model.User
 import dev.oskarjohansson.repository.UserRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 
 @Service
 class AdminService(private val userRepository: UserRepository) {
 
 
-    fun getUsers(): List<User>{
-
-        return userRepository.findAll()
-
+    fun getUsers(pageable: Pageable): Page<User> {
+        return userRepository.findAll(pageable)
     }
 }
